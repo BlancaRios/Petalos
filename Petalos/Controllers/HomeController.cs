a5999893de9161c7c433d32c0561c5c4f33bc240
BlancaRios <71105554+BlancaRios@users.noreply.github.com>
2021-12-27 16:19:24 -0600

    Agregar archivos de proyecto.

---
+++ Petalos/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Petalos.Models;
+using Petalos.Models.ViewModels;
+
+namespace Petalos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public floresContext Context { get; set; }
+        public HomeController(floresContext context)
+        {
+            Context = context;
+        }
+        public IActionResult Index()
+        {
+            var f = Context.Datosflores.OrderBy(x => x.Nombre);
+            return View(f);
+        }
+        [Route("informacion/{nombre}")]
+        public IActionResult Flor(string nombre)
+        {
+            var f = Context.Datosflores.Include(x=>x.Imagenesflores).FirstOrDefault(x => x.Nombre == nombre.Replace("-", " "));
+            if (f==null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                FloresViewModel vm = new();
+                vm.FlorUnica = f;
+                Random rnd = new();
+                vm.CuatroFlores=Context.Datosflores                  
+                    .Where(x => x.Idflor != f.Idflor)
+                    .ToList()
+                    .OrderBy(x => rnd.Next())
+                    .Take(4)
+                    .Select(x => new Datosflore { Idflor = x.Idflor, Nombre = x.Nombre });
+                return View(vm);
+            }
+           
+        }
+    }
+}
---
+++ Petalos/Models/floresContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Petalos.Models
+{
+    public partial class floresContext : DbContext
+    {
+        public floresContext()
+        {
+        }
+
+        public floresContext(DbContextOptions<floresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Datosflore> Datosflores { get; set; }
+        public virtual DbSet<Imagenesflore> Imagenesflores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Datosflore>(entity =>
+            {
+                entity.HasKey(e => e.Idflor)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("datosflores");
+
+                entity.HasCharSet("latin1")
+                    .UseCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Idflor).HasColumnName("idflor");
+
+                entity.Property(e => e.Descripcion)
+                    .IsRequired()
+                    .HasColumnType("text")
+                    .HasColumnName("descripcion");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(45)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.Nombrecientifico)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("nombrecientifico");
+
+                entity.Property(e => e.Nombrecomun)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .HasColumnName("nombrecomun");
+
+                entity.Property(e => e.Origen)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .HasColumnName("origen");
+            });
+
+            modelBuilder.Entity<Imagenesflore>(entity =>
+            {
+                entity.HasKey(e => e.Idimagen)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("imagenesflores");
+
+                entity.HasCharSet("latin1")
+                    .UseCollation("latin1_swedish_ci");
+
+                entity.HasIndex(e => e.Idflor, "FK_imagenesflores_1");
+
+                entity.Property(e => e.Idimagen).HasColumnName("idimagen");
+
+                entity.Property(e => e.Idflor).HasColumnName("idflor");
+
+                entity.Property(e => e.Nombreimagen)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("nombreimagen");
+
+                entity.HasOne(d => d.IdflorNavigation)
+                    .WithMany(p => p.Imagenesflores)
+                    .HasForeignKey(d => d.Idflor)
+                    .HasConstraintName("FK_imagenesflores_1");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
